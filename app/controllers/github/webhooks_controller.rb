fdc9ce245407ffa6eaa1c261fc23e18034614924
Ted Johansson <drenmi@gmail.com>
2015-07-16 20:55:48 +0800

    Consolidate Github::Controller into Github::WebhooksController

--- app/controllers/github/webhooks_controller.rb
+++ app/controllers/github/webhooks_controller.rb
@@ -1,9 +1,17 @@
 module Github
-  class WebhooksController < Github::Controller
+  class WebhooksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     respond_to :json
 
     def push_event
       render_status :ok
+    end
+
+    private
+
+    def render_status(status)
+      render nothing: true, status: status
     end
   end
 end
