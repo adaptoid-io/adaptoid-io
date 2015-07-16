488f75cc6f7a6e940a9e40d20a06cfdce475840b
Ted Johansson <drenmi@gmail.com>
2015-07-16 22:20:00 +0800

    Fix RuboCop offenses

--- spec/github/webhooks/push_event_spec.rb
+++ spec/github/webhooks/push_event_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe Github::Webhooks::PushEvent do
-  let(:payload) { { ref: 'refs/heads/master', commits: [{ author: 'Yoda', added: ['foo.md'] }, { author: 'Obi-Wan', added: ['bar.md'] } ] } }
+  let(:payload) { { ref: 'refs/heads/master', commits: [{ author: 'Yoda', added: ['foo.md'] }, { author: 'Obi-Wan', added: ['bar.md'] }] } }
 
   describe '.parse' do
     subject(:result) { described_class.parse(payload) }
 
     context 'with valid payload' do
       it { expect(result.branch).to eq('master') }
-      it { expect(result.authors).to eq(%w{Yoda Obi-Wan}) }
-      it { expect(result.files.added).to eq(%w{foo.md bar.md}) }
+      it { expect(result.authors).to eq(%w(Yoda Obi-Wan)) }
+      it { expect(result.files.added).to eq(%w(foo.md bar.md)) }
     end
 
     context 'without a valid ref' do
