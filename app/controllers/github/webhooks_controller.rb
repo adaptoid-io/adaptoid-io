d80a2404496b66d198b7787e13f29dca0aae8540
Ted Johansson <drenmi@gmail.com>
2015-07-16 21:26:18 +0800

    Implement token authentication for webhooks

--- app/controllers/github/webhooks_controller.rb
+++ app/controllers/github/webhooks_controller.rb
@@ -1,6 +1,7 @@
 module Github
   class WebhooksController < ApplicationController
     skip_before_action :verify_authenticity_token
+    before_action :verify_signature
 
     respond_to :json
 
@@ -12,6 +13,11 @@ module Github
 
     def render_status(status)
       render nothing: true, status: status
+    end
+
+    def verify_signature
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], request.body.read)
+      render_status(:error) unless Rack::Utils.secure_compare(signature, request.headers['HTTP_X_HUB_SIGNATURE'])
     end
   end
 end
