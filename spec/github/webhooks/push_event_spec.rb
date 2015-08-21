5543dc6189036786aae50f89856572be17a107a6
Ted Johansson <drenmi@gmail.com>
2015-08-21 19:21:07 +0800

    Improve tests for Github Webhook parser

--- spec/github/webhooks/push_event_spec.rb
+++ spec/github/webhooks/push_event_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 describe Github::Webhooks::PushEvent do
-  let(:payload) { { ref: 'refs/heads/master', commits: [{ author: 'Yoda', added: ['foo.md'] }, { author: 'Obi-Wan', added: ['bar.md'] }] } }
+  let(:payload) do
+    {
+      ref: 'refs/heads/master',
+      commits: [
+        { author: 'Yoda', added: %w(foo.md bar.md), modified: [] },
+        { author: 'Obi-Wan', added: %w(baz.md), modified: %w(foo.md) }
+      ]
+    }
+  end
 
   describe '.parse' do
     subject(:result) { described_class.parse(payload) }
@@ -9,7 +17,10 @@ describe Github::Webhooks::PushEvent do
     context 'with valid payload' do
       it { expect(result.branch).to eq('master') }
       it { expect(result.authors).to eq(%w(Yoda Obi-Wan)) }
-      it { expect(result.files.added).to eq(%w(foo.md bar.md)) }
+      it { expect(result.files.added.map(&:name)).to eq(%w(foo.md bar.md baz.md)) }
+      it { expect(result.files.added.map(&:slug)).to eq(%w(foo bar baz)) }
+      it { expect(result.files.modified.map(&:name)).to eq(%w(foo.md)) }
+      it { expect(result.files.modified.map(&:slug)).to eq(%w(foo)) }
     end
 
     context 'without a valid ref' do
