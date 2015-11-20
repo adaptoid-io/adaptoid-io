aa560d431db5bff7e9e022803f255930acc1ec2f
Ted Johansson <drenmi@gmail.com>
2015-11-20 18:40:45 +0800

    Augment CreatePost to download remote file content

--- spec/services/create_post_spec.rb
+++ spec/services/create_post_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 describe CreatePost, type: :service do
-  let(:file) { double('file', slug: 'on-the-road') }
+  let(:file) { double('file', slug: 'on-the-road', uri: '') }
 
   describe '#call' do
     subject(:service_call) { described_class.new(file).call }
 
+    before { allow(Net::HTTP).to receive(:get) { 'Hello world!' } }
+
     context 'with valid parameters' do
-      it { expect(service_call).to be_success }
+      context 'when fetching remote data is successful' do
+        it { expect(service_call).to be_success }
+      end
     end
 
     context 'with invalid parameters' do
