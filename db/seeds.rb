711dcb996f01da5d00322f52376d095cbf60da63
Ted Johansson <drenmi@gmail.com>
2016-04-01 11:13:56 +0200

    Add more seeds for Author and Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
-post = { title: 'On The Road', publication_date: Date.today - 1.day, body: '# On The Road', slug: 'on-the-road' }
+puts "Seeding Authors ..."
 
-Post.create(post)
+Author.create!(name: "Jaryl Sim")
+Author.create!(name: "Ted Johansson")
+Author.create!(name: "Matthew Yeow")
+
+puts "Seeding Posts ..."
+
+Post.create(title: "Disciplined Rails", publication_date: Date.today - 1.day, body: "# Disciplined Rails", slug: "disciplined-rails", authors: [Author.first])
+Post.create(title: "Good Cop, Bad Cop", publication_date: Date.today - 2.weeks, body: "# Good Cop, Bad Cop", slug: "good-cop-bad-cop", authors: [Author.second])
+Post.create(title: "Tinkerbox Value Objects", publication_date: Date.today - 2.months, body: "# Tinkerbox Value Objects", slug: "tinkerbox-value-objects", authors: Author.all)
